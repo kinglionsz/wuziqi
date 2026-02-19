cea8128d4ae36e2b3cdfb1963eaf142c7f095658
Developer <dev@example.com>
2026-02-20 06:35:56 +0800

    fix: 更新页脚版本号为v0.3

--- src/App.jsx
+++ src/App.jsx
@@ -756,7 +756,7 @@ function App() {
       </audio>
 
       <div className="footer">
-        <p>©️狮王李 保留所有权利 2026 | 版本 v0.2</p>
+        <p>©️狮王李 保留所有权利 2026 | 版本 v0.3</p>
       </div>
     </div>
   )
