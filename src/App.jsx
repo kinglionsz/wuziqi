2856056a899dc49c44c71df4ced0de42b061bbf0
Developer <dev@example.com>
2026-02-23 13:08:47 +0800

    AI 智能对手部分新增三种难度详细说明 部署信息更新为 v0.51 版本历史新增 v0.51 变更记录

--- src/App.jsx
+++ src/App.jsx
@@ -253,7 +253,7 @@ function App() {
       </audio>
 
       <div className="footer">
-        <p>©️狮王李 保留所有权利 2026 | 版本 v0.5</p>
+        <p>©️狮王李 保留所有权利 2026 | 版本 v0.51</p>
       </div>
     </div>
   )
