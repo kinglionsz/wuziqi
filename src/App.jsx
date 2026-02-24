2203c14c5d1995a5ec8ab11207308f261534252a
Developer <dev@example.com>
2026-02-24 12:52:49 +0800

    fix: 更新版本号至 v0.6

--- src/App.jsx
+++ src/App.jsx
@@ -348,7 +348,7 @@ function App() {
       </audio>
 
       <div className="footer">
-        <p>©️狮王李 保留所有权利 2026 | 版本 v0.51</p>
+        <p>©️狮王李 保留所有权利 2026 | 版本 v0.6</p>
       </div>
     </div>
   )
